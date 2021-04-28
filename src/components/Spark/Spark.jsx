a11d5c0aa528283193d3e2356ce4cadcba0063a0
Siequnu <11821606+Siequnu@users.noreply.github.com>
2021-04-28 22:38:50 +0800

    Add new loading method

--- src/components/Spark/Spark.jsx
+++ src/components/Spark/Spark.jsx
@@ -1,59 +1,51 @@
 import React from 'react';
+import useSWR from 'swr'
 
 import BubbleUI from "react-bubble-ui";
 import "react-bubble-ui/dist/index.css";
 
 import ActivityBubble from "./ActivityBubble";
 
+import { css } from "@emotion/core";
+import BounceLoader from "react-spinners/BounceLoader";
+
 import "./Spark.css";
 
-class Spark extends React.Component {
-    constructor(props) {
-        super(props);
+function Spark() {
 
-        this.options = {
-            size: 165,
-            minSize: 20,
-            gutter: 10,
-            provideProps: true,
-            numCols: 5,
-            fringeWidth: 120,
-            yRadius: 200,
-            xRadius: 100,
-            cornerRadius: 50,
-            showGuides: false,
-            compact: true,
-            gravitation: 5
-        }
-
-        this.state = {
-            activities: [],
-        }
-
+    const override = css`display: block; margin: 0 auto;`;
+    
+    const options = {
+        size: 165,
+        minSize: 20,
+        gutter: 10,
+        provideProps: true,
+        numCols: 5,
+        fringeWidth: 120,
+        yRadius: 200,
+        xRadius: 100,
+        cornerRadius: 50,
+        showGuides: false,
+        compact: true,
+        gravitation: 5
     }
 
-    componentDidMount() {
-        this.getActivities();
-    }
+    const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
+    const { data } = useSWR('/activities/api/get', fetcher)
 
-    getActivities = () => {
-        fetch('/activities/api/get').then(res => res.json()).then(data => {
-            this.setState({ activities: data.activities });
-        })
-    }
+    if (!data) return <BounceLoader color={"#F19820"} loading={true} css={override} size={100} />
 
-    render() {
-        return (
-            <BubbleUI options={this.options} className="sparkBubbleUi">
-                { this.state.activities.map((activity, i) => {
-                    return (
-                        <ActivityBubble key={i} activityId={i} title={activity.title} thumbnail={activity.thumbnail}/>
-                    )
-                })}
-            </BubbleUI>
-        )
-    }
+    return (
+        <BubbleUI options={options} className="sparkBubbleUi">
+            { data.activities.map((activity, i) => {
+                return (
+                    <ActivityBubble key={i} activityId={i} title={activity.title} thumbnail={activity.thumbnail} />
+                )
+            })}
+        </BubbleUI>
+    )
+
 }
 
 export default Spark;
--- src/components/ViewActivities/ViewActivitiesComponent.jsx
+++ src/components/ViewActivities/ViewActivitiesComponent.jsx
@@ -1,27 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React from "react"
+import useSWR from 'swr'
 import './ViewActivitiesComponent.css'
+
+import { css } from "@emotion/core";
+import BounceLoader from "react-spinners/BounceLoader";
 
 import CardComponent from '../Card/CardComponent';
 import Onboarding from '../Onboarding/Onboarding'
 
+
 function ViewActivitiesComponent() {
 
-    const [activities, setActivities] = useState([]);
+    const override = css`display: block; margin: 0 auto;`;
 
-    useEffect(() => {
-        getActivities();
-    }, [])
+    const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
-    const getActivities = () => {
-        fetch('/activities/api/get').then(res => res.json()).then(data => {
-            setActivities(data.activities);
-        });
-    }
+    const { data } = useSWR('/activities/api/get', fetcher)
+
+    if (!data) return <BounceLoader color={"#F19820"} loading={true} css={override} size={100} />
 
     return (
         <div className="ViewActivitiesComponent">
             <Onboarding />
-            <CardComponent cards={activities} />
+            <CardComponent cards={data.activities} />
         </div>
     )
 }
