c5c14c0807f9ecee4bf86631e7b51f208ebf5ee2
Siequnu <11821606+Siequnu@users.noreply.github.com>
2021-05-17 22:33:48 +0800

    Fix video thumbnail

--- src/components/Activity/ActivityIntroduction.jsx
+++ src/components/Activity/ActivityIntroduction.jsx
@@ -6,7 +6,11 @@ import './ActivityIntroduction.css';
 export default function ActivityIntroduction(props) {
     return (
         <div className="ActivityIntroduction">
-            <img src={'/activities/' + (props.activityId) + '/' + props.activity.thumbnail} alt="Header decoration" />
+            {(props.activity.thumbnail.split('.').pop() === 'mp4') ? (
+                <video src={'/activities/' + (props.activityId) + '/' + props.activity.thumbnail} alt="Header decoration" />
+            ) : (
+                <img src={'/activities/' + (props.activityId) + '/' + props.activity.thumbnail} alt="Header decoration" />
+            )}
             <h1>{props.activity.title}</h1>
             <p dangerouslySetInnerHTML={{ __html: props.activity.description }}></p>
             <Button variant="contained" color="primary" onClick={props.handleStartActivity}>
