48132b7d91d4ac65dbdfe0b7c56842d98d42d8a4
Siequnu <11821606+Siequnu@users.noreply.github.com>
2021-05-18 14:03:00 +0800

    Update colours

--- api/app/activities/routes.py
+++ api/app/activities/routes.py
@@ -1,35 +1,34 @@
-from flask import abort, current_app, request, jsonify
+from flask import jsonify
 from . import bp
 import json
-from datetime import datetime
 from colorthief import ColorThief
 
 
 """
-Class helpers
+Class helpers for the Activities module
 """
 
 
 def write_activities_json(activities_object):
-	try:
-		activities_file = open('../public/activities/activities.json', 'w')
-		json.dump(activities_object, activities_file)
-		activities_file.close()
-	except Exception as e:
-		print('An error occured while trying to write the activities.json file')
-		print(e)
+    try:
+        activities_file = open('../src/activities/activities.json', 'w')
+        json.dump(activities_object, activities_file)
+        activities_file.close()
+    except Exception as e:
+        print('An error occured while trying to write the activities.json file')
+        print(e)
 
 
 def get_activities_json():
-	try:
-		activities_file = open('../public/activities/activities.json')
-		activities = json.load(activities_file)
-		return activities
+    try:
+        activities_file = open('../src/activities/activities.json')
+        activities = json.load(activities_file)
+        return activities
 
-	except Exception as e:
-		print('An error occured while trying to load the activities.json file')
-		print(e)
-		return False
+    except Exception as e:
+        print('An error occured while trying to load the activities.json file')
+        print(e)
+        return False
 
 
 def rgb2hex(color):
@@ -47,40 +46,45 @@ def rgb2hex(color):
 """
 Startup routes
 """
-# At startup, calculate all colours
 
 
 def get_dominant_colour(image_path):
-	color_thief = ColorThief(image_path)
-	dominant_color = color_thief.get_color(quality=1)
-	return dominant_color
+    """
+    Calculates the dominant colours of activity images
+    """
+    color_thief = ColorThief(image_path)
+    dominant_color = color_thief.get_color(quality=1)
+    return dominant_color
 
 
+# DEBUG
 OVERWRITE_BACKGROUND_COLOURS = False
 
 
 def calculate_and_write_background_colours():
-	try:
-		activities = get_activities_json()
+    try:
+        activities = get_activities_json()
 
-		path_prefix = '../public/activities/'
-		activity_id = 1
-		for activity in activities:
-			if 'background_colour' not in activity or OVERWRITE_BACKGROUND_COLOURS:
-				thumbnail_path = path_prefix + \
-					str(activity_id) + '/' + activity['thumbnail']
-				colour = get_dominant_colour(thumbnail_path)
-				colour_hex = rgb2hex(colour)
-				activities[activity_id - 1]['background_colour'] = colour_hex
+        path_prefix = '../src/activities/'
+        activity_id = 1
+        for activity in activities:
+            print(f"Generating for {activity_id}")
+            if 'background_colour' not in activity or OVERWRITE_BACKGROUND_COLOURS:
+                thumbnail_path = path_prefix + \
+                    str(activity_id) + '/' + activity['thumbnail']
+                colour = get_dominant_colour(thumbnail_path)
+                colour_hex = rgb2hex(colour)
+                activities[activity_id - 1]['background_colour'] = colour_hex
 
-			activity_id += 1
+            activity_id += 1
 
-		write_activities_json(activities)
-		print('Successfully finished processing.')
+        write_activities_json(activities)
+        print('Successfully finished processing.')
 
-	except Exception as e:
-		print('An error occured while processing the activity background colours.')
-		print(e)
+    except Exception as e:
+        print('An error occured while \
+            processing the activity background colours.')
+        print(e)
 
 
 print('Activities startup task: generating activity background colours.')
@@ -94,23 +98,24 @@ Api routes
 
 @bp.route("/api/list")
 def get_all_activities():
-	"""
-	Get all the activities
-	"""
-	activities_file = open('../public/activities/activities.json')
-	activities = json.load(activities_file)
-	return jsonify({'activities': activities})
+    """
+    Get all the activities
+    """
+    activities_file = open('../src/activities/activities.json')
+    activities = json.load(activities_file)
+    return jsonify({'activities': activities})
 
 
 @bp.route("/api/get/<int:activity_id>")
 def get_single_activity(activity_id):
-	"""
-	Get single activity
-	"""
-	activities_file = open('../public/activities/activities.json')
-	activities = json.load(activities_file)
-	try:
-		activity = activities[activity_id]
-	except Exception as e:
-		return jsonify({'error', 'An error occured.'})
-	return jsonify({'activity': activity})
+    """
+    Get single activity
+    """
+    activities_file = open('../src/activities/activities.json')
+    activities = json.load(activities_file)
+    try:
+        activity = activities[activity_id]
+    except Exception as error:
+        print(error)
+        return jsonify({'error', 'An error occured.'})
+    return jsonify({'activity': activity})
