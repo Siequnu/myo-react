7130312d6687aa09af456d4fed243fbaccf7112e
Siequnu <11821606+Siequnu@users.noreply.github.com>
2021-05-18 13:18:51 +0800

    Develop user page

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -1,13 +1,15 @@
 import React, { Suspense } from 'react';
 import './SplashScreen.css';
 
+import 'animate.css'
+
 export default function SplashScreen() {
   return (
     <div className="SplashScreen">
       <div className="SplashScreenWrapper">
         <Suspense>
           <img src="/assets/myo-text-icon.png" alt="Myo icon" />
-          <p>Spark wonder</p>
+          <p className="animate__animated animate__bounceIn">Spark wonder</p>
         </Suspense>
       </div>
     </div>
--- src/components/UserMenu/UserHero.jsx
+++ src/components/UserMenu/UserHero.jsx
@@ -5,12 +5,15 @@ import useSWR from 'swr'
 import { css } from "@emotion/core";
 import BounceLoader from "react-spinners/BounceLoader";
 
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import LocalActivityIcon from '@material-ui/icons/LocalActivity';
+
 import Logout from '../Auth/Logout';
 export default function UserHero(props) {
 
     const { data } = useSWR('/auth/profile')
     const bounceLoaderCss = css`display: block; margin: 0 auto;`;
-    
+
     if (!props.activities) return null;
     if (!data) return <BounceLoader color={"#F19820"} loading={true} css={bounceLoaderCss} size={100} />
 
@@ -18,14 +21,18 @@ export default function UserHero(props) {
     const thumbnail = `/activities/${activity.activityId}/${activity.thumbnail}`
 
     return (
-        <div className="UserHero" style={{ backgroundImage: `url("${thumbnail}"`}}>
-            <div className="UserHeroContent" style={{'background': `linear-gradient(90deg, rgba(241,152,32,1) 40%, rgba(255,255,255,0) 90%)`}}>
+        <div className="UserHero" style={{ backgroundImage: `url("${thumbnail}"` }}>
+            <div className="UserHeroContent" style={{ 'background': `linear-gradient(90deg, rgba(241,152,32,1) 40%, rgba(255,255,255,0) 90%)` }}>
+                
+                <Logout className="UserHeroLink" />
                 <h1>{data.username}</h1>
-                <p>Member since:</p>
-                <p>{data.registered}</p>
+                <p><AccountCircleIcon className="Icon" /> Member since:</p>
+                <p className="caption">{data.registered}</p>
 
-                <Logout className="UserHeroLink"/>   
-
+                <p><LocalActivityIcon className="Icon" /> Total activities:</p>
+                <p className="caption">52</p>
+                <span className="stamp is-nope">Super member</span>
+                
             </div>
         </div>
     )
