fbf724a48b3887e20ebe6c55160d4d2b941c97ea
Siequnu <11821606+Siequnu@users.noreply.github.com>
2021-06-15 01:39:32 +0800

    Add Capacitor iOS port

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myo.app',
+  appName: 'Myo',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
