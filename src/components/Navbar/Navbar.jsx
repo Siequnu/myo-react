5be79e5bc7a21ba63ab4409f9ae12c9233636dd7
Siequnu <11821606+Siequnu@users.noreply.github.com>
2021-06-15 18:43:14 +0800

    Adjust margins for iOS

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,8 +10,17 @@ import HomeIcon from '@material-ui/icons/Home';
 import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 import BrushIcon from '@material-ui/icons/Brush';
 
+import { Device } from '@capacitor/device';
+
 export default function SimpleBottomNavigation() {
     const [value, setValue] = React.useState(0);
+
+    const [devicePlatform, setDevicePlatform] = React.useState('web')
+    const getDeviceInfo = async () => {
+      const info = await Device.getInfo();
+      setDevicePlatform(info.platform)
+    };
+    getDeviceInfo()
   
     return (
       <BottomNavigation
@@ -21,6 +30,10 @@ export default function SimpleBottomNavigation() {
         }}
         showLabels
         className="navbar"
+        style={{
+          paddingBottom : devicePlatform === 'web' ? '0' : '20px',
+          height: devicePlatform === 'web' ? 'auto' : '80px'
+          }}
       >
         <BottomNavigationAction component={Link} to="/" id="HomeNavbarIcon" label="Home" icon={<HomeIcon />} />
         <BottomNavigationAction component={Link} to="/spark" id="SparkNavbarIcon" label="Spark" icon={<EmojiObjectsIcon />} />
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -5,21 +5,31 @@ import { Link } from '@material-ui/core';
 import './TopBar.css';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
-class TopBar extends React.Component {
-  
-  render() {
-      return (
-          <div className="TopBar">
-            <Link component={RouterLink} to="/" className="AccountLink">
-              <img className="MyoLogo" src="/assets/logo.png" alt="Myo app logo" />
-            </Link>
-            <img className="MyoText" src="/assets/myo-text.png" alt="Myo app logo" />
-            <Link component={RouterLink} to="/user" className="AccountLink">
-              <AccountCircleIcon fontSize="large" className="AccountCircle" />
-            </Link>
-          </div>
-      )
-  }
+import { Device } from '@capacitor/device';
+
+export default function TopBar(props) {
+
+  const [devicePlatform, setDevicePlatform] = React.useState('web')
+  const getDeviceInfo = async () => {
+    const info = await Device.getInfo();
+    setDevicePlatform(info.platform)
+  };
+  getDeviceInfo()
+
+  return (
+    <div className="TopBar" style={{
+        paddingTop : devicePlatform === 'web' ? '0' : '35px',
+        height: devicePlatform === 'web' ? '75px' : '75px'
+        }}
+      >
+      <Link component={RouterLink} to="/" className="AccountLink">
+        <img className="MyoLogo" src="/assets/logo.png" alt="Myo app logo" />
+      </Link>
+      <img className="MyoText" src="/assets/myo-text.png" alt="Myo app logo" />
+      <Link component={RouterLink} to="/user" className="AccountLink">
+        <AccountCircleIcon fontSize="large" className="AccountCircle" />
+      </Link>
+    </div>
+  )
 }
 
-export default TopBar;
